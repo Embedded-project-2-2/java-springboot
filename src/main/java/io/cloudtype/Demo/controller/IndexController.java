45bdbc1e1586fdbed53f81b9579d72e1cb57fcb2
hongj <rick0201@naver.com>
2024-09-27 09:02:31 +0900

    테스트 모델 추가

--- src/main/java/io/cloudtype/Demo/controller/IndexController.java
+++ src/main/java/io/cloudtype/Demo/controller/IndexController.java
@@ -1,8 +1,14 @@
 package io.cloudtype.Demo.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.MalformedURLException;
 
 @Controller
 public class IndexController {
@@ -10,5 +16,12 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @ResponseBody
+    @PostMapping("/item/{imageFiles}")
+    public Resource showImage(@PathVariable String imageFiles) throws
+            MalformedURLException {
+        return new UrlResource()
     }
 }
